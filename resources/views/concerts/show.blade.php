164afb2f391682398739ef86f994052b2db57b57
EsthephanyAlcantar <fany.alcantarg@gmail.com>
2026-05-17 03:19:42 -0600

    Cargar estilos desde public

--- resources/views/concerts/show.blade.php
+++ resources/views/concerts/show.blade.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $concert->artista }}</title>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('app.css') }}">
+
 </head>
 
 <body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Concert Memories</title>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('app.css') }}">
 
 </head>
 
