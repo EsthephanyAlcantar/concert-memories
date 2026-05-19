00edf750a5b9823196b982306f01a23086a42c68
EsthephanyAlcantar <fany.alcantarg@gmail.com>
2026-05-18 18:53:07 -0600

    Corregir carga de CSS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <title>Concert Memories</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('app.css') }}">
+    <link rel="stylesheet" href="/app.css">
 
 </head>
 
